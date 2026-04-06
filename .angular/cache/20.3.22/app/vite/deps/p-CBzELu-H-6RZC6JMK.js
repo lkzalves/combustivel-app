2eb26267c0cdd2795df804337cc8f40f55daca58
Lucas Medeiros <lucas.a.medeiros1@gmail.com>
2026-04-05 21:40:47 -0300

    Teste

---
+++ .angular/cache/20.3.22/app/vite/deps/p-CBzELu-H-6RZC6JMK.js
@@ -0,0 +1,13 @@
+import {
+  a,
+  l
+} from "./chunk-MBMEVYYI.js";
+import "./chunk-QW6VVR4E.js";
+import "./chunk-S7GHTZCP.js";
+import "./chunk-EMI7KDEO.js";
+import "./chunk-WH2RCB7C.js";
+import "./chunk-WDMUDEB6.js";
+export {
+  l as iosTransitionAnimation,
+  a as shadow
+};
---
+++ .angular/cache/20.3.22/app/vite/deps/p-CU1SSH8_-OSVPE5XN.js
@@ -0,0 +1,11 @@
+import {
+  i
+} from "./chunk-M2SA24MP.js";
+import "./chunk-QW6VVR4E.js";
+import "./chunk-S7GHTZCP.js";
+import "./chunk-EMI7KDEO.js";
+import "./chunk-WH2RCB7C.js";
+import "./chunk-WDMUDEB6.js";
+export {
+  i as mdTransitionAnimation
+};
